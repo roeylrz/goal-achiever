ccd8d7ec6d9e3287338d8e257e39c8e470b8ad42
roey liraz <roeyliraz@gmail.com>
2022-05-15 00:31:24 +0300

    Add user and goal controller layer

---
+++ goal-achiever-server/src/controllers/userController.js
@@ -0,0 +1,6 @@
+import { User } from '../models';
+
+export const signup = async (req, res, next) => {};
+
+export const login = async (req, res, next) => {};
+export const logout = async (req, res, next) => {};
