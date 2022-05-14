180a89d7474ea79a81b806932f7c6f564beb08be
roey liraz <roeyliraz@gmail.com>
2022-05-15 00:15:37 +0300

    Add token validation

--- goal-achiever-server/src/authentication/token.js
+++ goal-achiever-server/src/authentication/token.js
@@ -4,3 +4,18 @@ import { jwt_secret, jwt_expire_within } from '../config';
 export const createWebToket = (payload) => {
   return jwt.sign(payload, jwt_secret, { expiresIn: jwt_expire_within });
 };
+
+export const validateToken = (accessToken) => {
+  return new Promise((resolve, reject) => {
+    jwt.verify(accessToken, jwt_secret, function (err, decodedPayload) {
+      if (err) {
+        return reject(err);
+      }
+      const payload = {
+        ...decodedPayload,
+        token: accessToken
+      };
+      resolve(payload);
+    });
+  });
+};
