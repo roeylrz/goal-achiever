13bff459b10bc266b57fdc975b07bb72dd175bb3
roey liraz <roeyliraz@gmail.com>
2022-05-15 00:08:41 +0300

    Add goal model layer

---
+++ goal-achiever-server/src/models/goalModel.js
@@ -0,0 +1,48 @@
+import mongoose from 'mongoose';
+
+const goalSchema = new mongoose.Schema({
+  Name: {
+    required: true,
+    type: String
+  },
+  Description: {
+    type: String,
+    default: ''
+  },
+  Owner: {
+    type: mongoose.Types.ObjectId,
+    ref: 'User',
+    required: true,
+    index: true
+  },
+  DueDate: {
+    type: Date,
+    default: null
+  },
+  Steps: {
+    type: [
+      {
+        Name: {
+          required: true,
+          type: String
+        },
+        Description: {
+          type: String,
+          default: ''
+        },
+        Completed: {
+          type: Boolean,
+          default: false
+        },
+        DueDate: {
+          type: Date,
+          default: null
+        }
+      }
+    ],
+    default: []
+  }
+});
+goalSchema.index({ 'Steps._id': 1 });
+const Goal = mongoose.model('Goal', goalSchema);
+export { Goal };
