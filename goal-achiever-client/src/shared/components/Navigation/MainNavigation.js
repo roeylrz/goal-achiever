5f4d28edd99e0109ed3155f6ca5018db34f3d0dc
roey liraz <roeyliraz@gmail.com>
2022-05-16 21:41:15 +0300

    Create NavLinks

--- goal-achiever-client/src/shared/components/Navigation/MainNavigation.js
+++ goal-achiever-client/src/shared/components/Navigation/MainNavigation.js
@@ -1,17 +1,29 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useContext, useMemo } from 'react';
+import { AuthContext } from '../../context/auth-context';
 import MainHeader from './MainHeader';
 import SideDrawer from './SideDrawer';
+import NavLinks from './NavLinks';
 
 const MainNavigation = () => {
-  return (
-    <Fragment>
-      <MainHeader>
-        <h2>Goal Achiever</h2>
-        <div>Logout</div>
-      </MainHeader>
-      <SideDrawer></SideDrawer>
-    </Fragment>
-  );
+  const auth = useContext(AuthContext);
+  const { isLoggedIn, logout } = auth;
+  const mainNavigation = useMemo(() => {
+    return (
+      <Fragment>
+        <MainHeader>
+          <h2>Goal Achiever</h2>
+          {isLoggedIn && <label onClick={logout}>Logout</label>}
+        </MainHeader>
+        {isLoggedIn && (
+          <SideDrawer>
+            <NavLinks />
+          </SideDrawer>
+        )}
+      </Fragment>
+    );
+  }, [isLoggedIn, logout]);
+
+  return mainNavigation;
 };
 
 export default MainNavigation;
---
+++ goal-achiever-client/src/shared/components/Navigation/NavLinks.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import * as routesConsts from '../../httpRequests/routes';
+import './NavLinks.scss';
+
+const NavLinkItem = ({ to, caption }) => (
+  <li>
+    <NavLink to={to}>{caption}</NavLink>
+    <span />
+  </li>
+);
+
+const NavLinks = () => {
+  return <ul className="nav-links"></ul>;
+};
+
+export default NavLinks;
