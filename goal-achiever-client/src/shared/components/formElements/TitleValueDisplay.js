c5e654c958f0eb90343c8060da5a5daa784a896a
roey liraz <roeyliraz@gmail.com>
2022-05-16 21:17:34 +0300

    Add TitleValueDisplay component

---
+++ goal-achiever-client/src/shared/components/formElements/TitleValueDisplay.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import classes from './TitleValueDisplay.module.scss';
+
+const TitleValueDisplay = ({ title, value }) => {
+  return (
+    <div className={classes.TitleValueDisplay}>
+      <label className={classes.TitleValueDisplay_title}>{title}</label>
+      <label className={classes.TitleValueDisplay_value}>
+        {(value && value) || 'N/A'}
+      </label>
+    </div>
+  );
+};
+
+export default TitleValueDisplay;
